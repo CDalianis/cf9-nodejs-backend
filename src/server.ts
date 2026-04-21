86fc50550d025b16d3907573975dd119ac49120c
CDalianis <christos_dl@hotmail.com>
2026-04-21 14:06:21 +0300

    update server ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import app from './app';
 import { connectDB } from './utils/db';
 
+const start = async () => {
+  try {
+    await connectDB();
+    
+    app.listen(3000, () => {
+      console.log('Server is up on port 3000');
+    });
 
-const start = async() => {
-  
-  await connectDB();
-  
-  app.listen(3000, ()=>{
-    console.log('Server is up');
-  })
-}
+  } catch (error) {
+    console.error('Failed to start server:', error);
+    process.exit(1);
+  }
+};
 
 start();
